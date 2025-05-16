92a6464b7d8e72c8eb3be6e9fb91e1cefb2d756b
tranvietthang251 <130987106+tranviethang251@users.noreply.github.com>
2025-05-16 11:09:12 +0700

    Update testbench

--- Traffic_Light/testbench/testbench.v
+++ Traffic_Light/testbench/testbench.v
@@ -1,6 +1,5 @@
 `timescale 1 ns/1 ps
 module testbench ; 
-  
 // localparameter 
 localparam pCLOCK_PERIOD_TIME = 100 ; 
 localparam pRED_CNT_INIT    = 18  ;
@@ -15,6 +14,8 @@ wire Red_Light ;
 wire Yellow_Light; 
 wire Green_Light ; 
 wire [pCount_Width-1:0] cnt_out_num ; 
+wire [6:0] tens_digit_led ; 
+wire [6:0] unit_digit_led ;
 // body 
 // CLOCK GENERATION
 always begin 
@@ -36,5 +37,6 @@ end
 Traffic_Light #(.CL_PERIOD_TIME(pCLOCK_PERIOD_TIME),.YELLOW_LIGHT_TIME(pYELLOW_CNT_INIT), 
                .RED_LIGHT_TIME(pRED_CNT_INIT), .GREEN_LIGHT_TIME(pGREEN_CNT_INIT)) 
                uut 
-               (.clk(clk),.rstb(rstb),.en(en),.red_light(Red_Light),.yellow_light(Yellow_Light),.green_light(Green_Light),.cnt_out_num(cnt_out_num)) ; 
+               (.clk(clk),.rstb(rstb),.en(en),.red_light(Red_Light),.yellow_light(Yellow_Light),.green_light(Green_Light),.cnt_out_num(cnt_out_num),
+               .tens_digit_led(tens_digit_led),.unit_digit_led(unit_digit_led)) ; 
 endmodule 
